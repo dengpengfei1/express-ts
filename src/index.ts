7609ae65a8241842e755f7b97ead910daa9f6cad
dengpengfei <dengpengfei@guazi.com>
2019-12-02 10:25:13 +0800

    express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('/:u', (req, res) => {
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
   res.json({
     url: req.url
   })
+})
+
+app.get('/download', (req, res) => {
+  res.download(path.resolve(__dirname, 't.txt'))
+})
+
+app.get('/:userId', (req, res) => {
+  res.json(req.params)
+})
+
+app.get('/x/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, function (req, res) {
+  res.send('Hello from B!')
 })
 
 app.listen(3000)
